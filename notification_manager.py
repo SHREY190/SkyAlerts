e6c69f27d079d4f985fb07814ced54795325cfc6
SHREY190 <92907792+SHREY190@users.noreply.github.com>
2023-07-20 21:03:02 +0530

    Update notification_manager.py

--- notification_manager.py
+++ notification_manager.py
@@ -1,5 +1,4 @@
 from twilio.rest import Client
-#Recovery code:  4DX-rtE8MYuIr92n4kPHciHRkWXDkz_4q9Mcs3Xd
 TWILIO_SID = "Twilio SID"
 TWILIO_AUTH_TOKEN = "Authentification Token"
 TWILIO_VIRTUAL_NUMBER = "Twilo Virtual Number"
